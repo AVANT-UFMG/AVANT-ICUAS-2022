e8d5257d3cecbfadcd96ce72b69059cad75717d9
Gabriel Lopes Machado <gabriel.galoma@gmail.com>
2022-03-08 00:12:18 -0300

    add: extremely strange input_pose usage

--- drone_controller/src/integration/src/tests/test_pub_input_pose.py
+++ drone_controller/src/integration/src/tests/test_pub_input_pose.py
@@ -1,12 +1,18 @@
 import rospy
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PoseStamped
 
 if __name__ == "__main__":
     rospy.init_node("test_input_pose_topic_by_code")
-    tracker_input_pose_publisher = rospy.Publisher("/red/tracker/input_pose", PointStamped, queue_size=10)
-    test_msg = PointStamped()
-    test_msg.point.x = 9.0
-    test_msg.point.y = 0.0
-    test_msg.point.z = 2.5
-    tracker_input_pose_publisher.publish(test_msg)
-    rospy.spin()
+    tracker_input_pose_publisher = rospy.Publisher("/red/tracker/input_pose", PoseStamped, queue_size=10)
+    test_msg = PoseStamped()
+    test_msg.pose.position.x = 9.0
+    test_msg.pose.position.y = 0.0
+    test_msg.pose.position.z = 2.5
+    rate = rospy.Rate(10)
+    i = 0
+    while True:
+        tracker_input_pose_publisher.publish(test_msg)
+        rate.sleep()
+        i = i + 1
+        if i > 1:
+            break
