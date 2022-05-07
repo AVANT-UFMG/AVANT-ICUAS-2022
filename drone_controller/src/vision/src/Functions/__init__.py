e604ea9058dd933d71684e962d462f91cc73411c
João Pedro <joaopedrom98@gmail.com>
2022-05-07 12:24:37 -0300

    adição função StablePosition

--- drone_controller/src/vision/src/Functions/__init__.py
+++ drone_controller/src/vision/src/Functions/__init__.py
@@ -1,5 +1,5 @@
 from .StrategiesFunctions import GetStrategies 
-from .MoveDroneFunctions import GetDronePoint, MoveToPoint, ArrivePoint, PublishDeadPoint
+from .MoveDroneFunctions import GetDronePoint, MoveToPoint, ArrivePoint, PublishDeadPoint, WaitStablePosition
 from .TagDetectorFunctions import GetArTag
 from .ImageFunctions import GetImageFromDrone, CameraCallback,FilterColorBlack,LabelImage, GetImageToPublish
 from .PoseEstimationFunctions import EstimatePointPosition
